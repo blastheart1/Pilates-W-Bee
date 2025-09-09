815b4b7289ae4dee6ec1adddd2e5cde965eed367
blastheart1 <antonioluis.santos1@gmail.com>
2025-09-10 04:24:40 +0800

    Minor update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -108,7 +108,7 @@ export default function SinglePageApp() {
       price: 4450,
       quantity: 1,
       image:
-        "src\PilatesShopImages\PilatesMat.jpg",
+        "PilatesShopImages/PilatesMat.jpg",
       type: "physical",
     },
     {
@@ -117,7 +117,7 @@ export default function SinglePageApp() {
       price: 2250,
       quantity: 2,
       image:
-        "src\PilatesShopImages\images.avif",
+        "PilatesShopImages/ResistanceBandsSet.jpg",
       type: "physical",
     },
   ]);
@@ -820,7 +820,7 @@ export default function SinglePageApp() {
         "123 Wellness Street, Mind & Body District, Makati City, Metro Manila",
       phone: "+63 (917) 123-4567",
       image:
-        "Pilatesstudioimages/Studio1.jpeg",
+        "Pilatesstudioimages/Studio1.png",
       amenities: [
         "Private Showers",
         "Changing Rooms",
@@ -833,7 +833,7 @@ export default function SinglePageApp() {
       address: "456 Serenity Ave, Wellness Quarter, Quezon City, Metro Manila",
       phone: "+63 (917) 123-4568",
       image:
-        "Pilatesstudioimages/Studio2.jpeg",
+        "Pilatesstudioimages/Studio2.png",
       amenities: [
         "Outdoor Terrace",
         "Juice Bar",
